4af570e30eae47ecd0eef6ae0afc9b8fd71246a6
quanvm-github <2051110@gmail.com>
2016-05-08 02:34:20 +0700

    update

--- thuchanh3/js/thuchanh3.js
+++ thuchanh3/js/thuchanh3.js
@@ -1,8 +1,7 @@
-function show(e)
+function show(index)
 {
 	var x = document.getElementsByClassName("movies_trailer_infor");
 	var y = document.getElementsByClassName("movies_img");
-	var index = e.getAttribute("value");
 	if(x[index].style.display == "block") {
 		x[index].style.display = "none";
 		y[index].src = "images/right.png";
