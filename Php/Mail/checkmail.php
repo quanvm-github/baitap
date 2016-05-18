e74efa0860f1318b6c8e57c834101d41cfee03fe
quanvm-github <2051110@gmail.com>
2016-05-18 17:18:35 +0700

    update

--- Php/Mail/checkmail.php
+++ Php/Mail/checkmail.php
@@ -1,14 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mail Content</title>
+</head>
+
+<body>
+    <?php
     if (isset($_POST["to"])) {
         $to = $_POST["to"];
         $subject = $_POST["subject"];
         $content = $_POST["content"];
-        $header = "From: webmaster@example.com";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= 'From: <webmaster@example.com>' . "\r\n";
         echo $to . "<br>";
         echo $subject . "<br>";
         echo $content . "<br>";
-        if (mb_send_mail($to, $subject, $content, $header)) {
+        if (mail($to, $subject, $content, $headers)) {
             echo "Your email has been sent !";
         }
         else echo "false";
     }
+?>
+<br>
+<a href="index.php">Back !</a>
+</body>
+
+</html>
